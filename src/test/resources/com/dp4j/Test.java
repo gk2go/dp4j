64045a786636cf90b472785d50b116772e8db9fe
simpatico <simpatico@simpatico-PC>
2011-02-05 23:22:40 +0100

    what was broken was simply the test including while(true). The compiler determined that all statements after it will not be reacheble. A raffle of errors is printed, apparently before Privateaccessor since the printed out is correct and the classes compile.

---
+++ src/test/resources/com/dp4j/Test.java
@@ -0,0 +1,48 @@
+package com.dp4j;
+
+class PrivateClass {
+
+    private int i;
+    public int j;
+    public static int gg;
+}
+
+class K {
+
+    protected int m;
+    PrivateClass pc = new PrivateClass();
+}
+
+public class Test extends K {
+
+    int jj;
+    int g;
+    int h;
+
+    @org.junit.Test
+    public void t() {
+        if (pc.i > 0) {
+            int g = pc.i;
+        }
+        double pi = 3.14;
+        final int fff = pc.i;
+        {
+            int f = pc.i;
+        }
+
+        while (2 < Math.random() * 5) {
+            int kk = pc.i;
+        }
+
+        PrivateClass privateClass = new PrivateClass();
+        int ff = privateClass.i;
+        pi = new PrivateClass().i;
+        int i = new PrivateClass().j;
+
+        try {
+            int kk = new PrivateClass().i;
+        } catch (Exception e) {
+            int kk = new PrivateClass().i;
+        }
+    }
+}
