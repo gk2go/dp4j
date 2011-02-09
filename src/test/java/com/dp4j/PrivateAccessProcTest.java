e411ebafaef3242aecde9f3d66b1c107a3128919
simpatico <simpatico@simpatico-PC>
2011-02-09 17:00:10 +0100

    Not anymore: At this point there's still the code to manually test (javac cmd).

--- src/test/java/com/dp4j/PrivateAccessProcTest.java
+++ src/test/java/com/dp4j/PrivateAccessProcTest.java
@@ -5,17 +5,12 @@
 package com.dp4j;
 
 import com.dp4j.processors.core.PrivateAccessProcessor;
-import com.dp4j.processors.*;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collection;
 import javax.annotation.processing.Processor;
 
-import javax.tools.JavaCompiler;
-import javax.tools.ToolProvider;
-import org.apache.commons.lang.StringUtils;
 import org.junit.Test;
 /**
  *
@@ -23,95 +18,10 @@ import org.junit.Test;
  */
 public class PrivateAccessProcTest extends AbstractAnnotationProcessorTest {
 
-    private static final JavaCompiler COMPILER = ToolProvider.getSystemJavaCompiler();
-
-    File getFile(final String... dirs) {
-        File ret = null;
-        for (String dir : dirs) {
-            if (ret == null) {
-                ret = new File(dir);
-            } else {
-                ret = new File(ret.getPath(), dir);
-            }
-        }
-        return ret;
-    }
-
-    static File getFile(final File dir, final String... dirs) {
-        File ret = dir;
-        for (String dir1 : dirs) {
-            if (ret == null) {
-                ret = new File(dir1);
-            } else {
-                ret = new File(ret.getPath(), dir1);
-            }
-        }
-        return ret;
-    }
-    File src = getFile(System.getProperty("user.dir"), "src", "main", "java");
-    final String procSrc = getFile(src.getAbsolutePath(), "com", "dp4j", "processors").getAbsolutePath();
-    static final File workingdir = new File(System.getProperty("user.dir"));
-    static final File testResources = getFile(workingdir, "src", "test", "resources");
-
-    final String getSrcFile(final Class clazz) {
-        return new File(src, clazz.getCanonicalName().replace(".", File.separator) + ".java").getAbsolutePath();
-    }
-
-    static String getTestFileAbsolutePath(final String className) {
-        return getFile(testResources, "com", "dp4j", "samples", className + ".java").getAbsolutePath();
-    }
-
-    final String getClassPath(final File dir, final Class clazz) {
-        return new File(dir, clazz.getCanonicalName().replace(".", File.separator) + ".class").getAbsolutePath();
-    }
-
     @org.junit.Test
     public void mostComprehensiveTest() throws IOException {
-        final Runtime runtime = Runtime.getRuntime();
-        runtime.traceInstructions(true);
-        runtime.traceMethodCalls(true);
-        File targetClasses = getFile(workingdir, "target", "classes");
-        File targetTestClasses = getFile(workingdir, "target", "test-classes");
-        final String junit = new File(testResources.getAbsolutePath(), "junit.jar").getAbsolutePath();
-        final String commons = new File(testResources.getAbsolutePath(), "commons.jar").getAbsolutePath();
-        String tools = getFile(System.getProperty("java.home")).getAbsolutePath();
-        int lastIndexOf = StringUtils.lastIndexOf(tools, File.separator);
-        tools = "\"" + getFile(tools.substring(0, lastIndexOf), "lib", "tools.jar").getAbsolutePath() + "\"";
-
-        String cp = getCp(tools, commons);
-        String javacCmd = "javac -d " + targetClasses + " ";
-
-        final String dp4jCompile = javacCmd + cp + getClassesToCompile(templateMethod.class,DProcessor.class,ExpProcResult.class ,PrivateAccessProcessor.class);
-
-        System.out.println(dp4jCompile);
-        cp = getCp(targetClasses.getAbsolutePath(), tools, commons, junit);
-        javacCmd = "javac -d " + targetTestClasses;
-        String testCmd = javacCmd + cp + " -processor " + PrivateAccessProcessor.class.getCanonicalName() + " " + getTestFileAbsolutePath("Test");
-        System.out.println(testCmd);
-        Process exec = runtime.exec(dp4jCompile, null, workingdir);
-
-        runtime.exec(testCmd, null, workingdir);
 
         assertCompilationSuccessful(compileTestCase("Test"));
-    }
-
-
-
-
-    private String getCp(final String... cmds) {
-        String ret = " -cp ";
-        for (String string : cmds) {
-            ret += string + File.pathSeparator;
-        }
-        return ret.substring(0, ret.length() - 1) + " ";
-    }
-
-    private String getClassesToCompile(final Class... cmds) {
-        String ret = " ";
-        for (Class string : cmds) {
-            ret += getSrcFile(string) + " ";
-        }
-        return ret;
     }
 
     @Test
