b029ca04c0572a5b823656b3c9f05b8c6794a6e9
simpatico.gabriele <simpatico.gabriele@localhost>
2011-08-20 08:30:22 +0300

    solves issue 7 by adding some missing documentation. Javadoc was added to the primary annotation of each pattern. The doc is !optimal (at least in comparison to previously committed, I think the singleton) but good enough for now.

--- src/main/java/com/dp4j/templateMethod.java
+++ src/main/java/com/dp4j/templateMethod.java
@@ -10,8 +10,18 @@ import java.lang.annotation.ElementType;
 
 
 /**
- *
- * @author simpatico
+<html>
+<body>
+
+<h4>Intent:</h4>
+<ul>
+  <li>lets subclasses redefine certain steps of an algorithm without changing the algorithm's structure.</li>
+  <li>Define the skeleton of an algorithm.</li>
+</ul>
+
+<img src="http://www.oodesign.com/images/design_patterns/behavioral/template_method_implementation_-_uml_class_diagram.gif" alt="Template Method Pattern Implementation UML Class Diagram"/>
+
+@see <a href="http://www.oodesign.com/composite%2pattern.html">Template Method</a>
  */
 @Documented
 @Target(ElementType.METHOD)
