73be88eefd690e350406b1351b55b3583d858f6d
gabriele <gabriele@mysimpatico.com>
2011-10-02 19:19:01 +0100

    test for dp4j issue with Thread.sleep

---
+++ dp4j-tests/src/test/java/com/dp4j/samples/ThreadSleepTest.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dp4j.samples;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author simpatico
+ */
+public class ThreadSleepTest {
+
+    @Test
+    public void testThreadSleep() throws InterruptedException{
+        Thread.sleep(1);
+    }
+}
